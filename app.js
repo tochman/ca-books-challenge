692e55c14597b1096cf20b12fe08e98eff073e16
Thomas <thomas@craftacademy.se>
2021-01-29 10:51:21 +0100

    adds cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const users = require('./routes/books');
 
@@ -10,6 +11,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:3474']
+}))
 
 app.use('/books', users);
 
